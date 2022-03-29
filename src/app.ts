87ade22f14bc9b308c48338a325fee8dcb18e678
m6keller <kellermatthewe@gmail.com>
2022-03-29 14:18:47 -0400

    error handling for delete functions

--- src/app.ts
+++ src/app.ts
@@ -59,22 +59,17 @@ app.get( "/api/animals/name/:name", (req: Request, res: Response, next ) => {
 });
  
 app.get( "/api/animals/id/:id", (req: Request, res: Response, next ) => {
-    const id: any = req.params.id;
-    if ( !id.isInteger() ) {
-        const error = new Error("Please input an integer value for the animal ID")
-        next( error.message );
-        return;
-    }
-   const query = "SELECT * FROM Animal_Data WHERE id = " + id;
-   connection.query( query, (err, rows) => {
-       if( err ) throw err;
-       if( rows.length === 0 ) {
-           const error = new Error("No animal with id " + id + " exists in the database")
-           next( error.message );
-           return;
-       }
-       return res.send( rows );
-   })
+    const id: number = +req.params.id;
+    const query = "SELECT * FROM Animal_Data WHERE id = " + id;
+    connection.query( query, (err, rows) => {
+        if( err ) throw err;
+        if( rows.length === 0 ) {
+            const error = new Error("No animal with id " + id + " exists in the database")
+            next( error.message );
+            return;
+        }
+        return res.send( rows );
+    })
 });
  
 router.post('/api/insert/animal', (req: Request, res: Response, next ) => {
@@ -133,44 +128,53 @@ router.post( '/api/update/animal', (req: Request, res: Response, next ) => {
  
  
 app.get('/api/delete/name/:name', function(req: Request, res: Response, next) {
-   var name = req.params.name;
-   var query = "DELETE FROM Animal_Data WHERE name = " + name;
-   connection.query(query, [name], function (err, result) {
-       if (err) throw err;
-       if( result.length === 0 ) {
+    var name: string = String(req.params.name);
+    var query = "SELECT * FROM Animal_Data WHERE name = \"" + name +"\"";
+    connection.query(query, [name], function (err, result) {
+        if (err) throw err;
+        if ( result.length === 0 ) {
             const error = new Error("this animal does not exist in the database")
-            next( error );
+            next( error.message );
             return;
-       }
-       console.log(result.affectedRows + " record(s) updated");
-       res.redirect('/');
+        }
+        else {
+            var query = "DELETE FROM Animal_Data WHERE name = \"" + name +"\"";
+            connection.query(query, [name], function (err, result) {
+                if (err) throw err;
+                console.log("record(s) updated");
+            });
+            return res.send("record updated");
+        }
    });
 });
  
 app.get('/api/delete/id/:id', function(req, res, next) {
     var id: any = req.params.id;
-        if ( !id.isInteger() ) {
-        const error = new Error("Please input an integer value for the animal ID")
+    try {
+        Number(id);
+    }
+    catch (e) {
+        const error = new Error("Please input a number")
         next( error.message );
         return;
-    }  
+    }        
     var check = "SELECT * FROM Animal_Data WHERE id = " + id;
     connection.query(check, [id], function (err, result) {
         if (err) throw err;
         if( result.length === 0 ) {
-            console.log( "result 0" );
             const error = new Error("No record of this animal in the database")
-            next( error );
+            next( error.message );
             return;
         }
+        else {
+            var query = "DELETE FROM Animal_Data WHERE id = " + id;
+            connection.query(query, [id], function (err, result) {
+                if (err) throw err;
+                console.log("record updated");
+            });
+            return res.send("record updated");
+        }
     });
-   var query = "DELETE FROM Animal_Data WHERE id = " + id;
-
-   connection.query(query, [id], function (err, result) {
-       if (err) throw err;
-       console.log(result.affectedRows + " record(s) updated");
-       return res.send( "/" );
-   });
 });
  
  
@@ -181,5 +185,3 @@ app.get( "/about", (req: Request, res: Response ) => {
 app.listen(port, () => {
    console.log("App is running on port " + port );
 });
- 
-
